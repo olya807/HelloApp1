b463e33c2c25ebe8d11bceb29541034f0280a3c3
Volha <morozolga92@gmail.com>
2023-03-15 21:27:11 +0300

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        int num = in.nextInt();
+
+        System.out.printf("Your number: %d \n", num);
+        in.close();
+    }
+}
